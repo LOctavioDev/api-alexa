5b4eb5a74b3f724e2c2cf71d1f3ddcf19e41e699
Luis Octavio <octaviodevtech@gmail.com>
2025-05-17 16:05:52 -0600

    add: routes

--- src/app.ts
+++ src/app.ts
@@ -8,10 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/tasks', taskRouter);
+
 app.use('/', (req, res) => {
   res.send('Welcome to the Task Manager API');
 });
-app.use('/api/tasks', taskRouter);
 
 connectDB();
 
